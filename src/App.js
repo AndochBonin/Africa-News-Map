080fad3eaf11127957547f3392b9018e6a5b9129
Andoch Bonin <andochbonin@gmail.com>
2021-04-16 02:25:20 +0000

    remove all non african countries, and fixed component sizes

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ import SidePanel from './components/SidePanel';
 function App() {
   return (
     <div className="App">
+
+      <div className='App-header'>
+        <h4>News Map</h4>
+      </div>
+
       <div className='Geochart'>
         <Geochart />
       </div>
     
-      <div className='SidePanel'>
+      <div className='SidePanel'> 
         <SidePanel />
       </div>
+
     </div>
   );
 }
--- src/components/Geochart.jsx
+++ src/components/Geochart.jsx
@@ -6,10 +6,10 @@ function Geochart() {
     return (
         <Chart
             width={'70vw'}
-            height={'100vh'}
+            height={'94.5vh'}
             chartType="GeoChart"
             data={[
-                ['Country', 'Outlook'],
+                ['Country', 'Average Temperature'],
                 ['Algeria', 36], ['Angola', -8], ['Benin', 6], ['Botswana', -24],
                 ['Burkina Faso', 12], ['Burundi', -3], ['Cameroon', 3],
                 ['CG', 2], ['Cape Verde', 15],
@@ -19,15 +19,14 @@ function Geochart() {
                 ['Egypt', 26], ['Equatorial Guinea', 3], ['Eritrea', 15],
                 ['Ethiopia', 9], ['Gabon', 0], ['Gambia', 13], ['Ghana', 5],
                 ['Guinea', 10], ['Guinea-Bissau', 12], ['Kenya', -1],
-                ['Lesotho', -29], ['Liberia', 6], ['Libya', 32], ['Madagascar', -12],
-                ['Madeira', 33], ['Malawi', -14], ['Mali', 12], ['Mauritania', 18],
-                ['Mauritius', -20], ['Mayotte', -13], ['Melilla', 35],
-                ['Morocco', 32], ['Mozambique', -25], ['Namibia', -22],
+                ['Lesotho', -29], ['Liberia', 6], ['Libya', 32], ['Madagascar', -12], 
+                ['Malawi', -14], ['Mali', 12], ['Mauritania', 18],
+                ['Mauritius', -20],['Morocco', 32], ['Mozambique', -25], ['Namibia', -22],
                 ['Niger', 14], ['Nigeria', 8], ['Republic of the Congo', -1],
-                ['Réunion', -21], ['Rwanda', -2], ['Saint Helena', -16],
+                ['Rwanda', -2], ['Saint Helena', -16],
                 ['São Tomé and Principe', 1], ['Senegal', 15],
                 ['Seychelles', -5], ['Sierra Leone', 8], ['Somalia', 2],
-                ['Sudan', 15], ['South Africa', -30], ['South Sudan', 5],
+                ['Sudan', 15], ['South Africa', -30], ['SS', 5],
                 ['Swaziland', -26], ['Tanzania', -6], ['Togo', 6], ['Tunisia', 34],
                 ['Uganda', 1], ['Western Sahara', 25], ['Zambia', -15],
                 ['Zimbabwe', -18],
@@ -36,8 +35,8 @@ function Geochart() {
             options = {{
                 region: '002',
                 colorAxis: {colors: ['#FF0000', 'FFFF00', '#00FF00']},
-                backgroundColor: '#282c34',
-                datalessRegionColor: '#282c34',
+                backgroundColor: '#282c40',
+                datalessRegionColor: '#282c40',
                 defaultColor: '#ffffff',
             }}
 
