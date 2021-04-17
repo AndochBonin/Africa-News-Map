fc6f1d3913a54332b11044ee3528c6e92d18f2f9
Andoch Bonin <andochbonin@gmail.com>
2021-04-17 15:26:22 +0000

    created environment variables for map api key and enabled geocoding. also made detail adjustments to frontend

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import Geochart from './components/Geochart';
 import SidePanel from './components/SidePanel';
 
 function App() {
+
   return (
     <div className="App">
 
       <div className='App-header'>
         <h4>News Map</h4>
       </div>
+      <div className='lineH'></div>
 
       <div className='Geochart'>
         <Geochart />
       </div>
-    
+      
+      
       <div className='SidePanel'> 
         <SidePanel />
       </div>
-
+      <div className='lineV'></div>
     </div>
   );
 }
--- src/components/Geochart.jsx
+++ src/components/Geochart.jsx
@@ -1,8 +1,19 @@
 import { Chart } from 'react-google-charts';
 import React from 'react';
 
+//let weather = require('openweather-apis');
+//weather.setLang('en');
+//weather.setAPPID("8e31489e1ab7b3cc29e9db1e90ac61ad");
+
+
+const { REACT_APP_GOOGLE_MAP_API } = process.env;
+
 function Geochart() {
     
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB0VO2dBzyfwZKQHZRSQm0fX0PhI0TissY&callback=initMap"
+    type="text/javascript"></script>
+    //weather.setCity("Algiers");
+    //weather.getTemperature(function(err, temp) { console.log(temp); } );
     return (
         <Chart
             width={'70vw'}
@@ -10,26 +21,21 @@ function Geochart() {
             chartType="GeoChart"
             data={[
                 ['Country', 'Average Temperature'],
-                ['Algeria', 36], ['Angola', -8], ['Benin', 6], ['Botswana', -24],
-                ['Burkina Faso', 12], ['Burundi', -3], ['Cameroon', 3],
-                ['CG', 2], ['Cape Verde', 15],
-                ['Central African Republic', 4], ['Ceuta', 35], ['Chad', 12],
-                ['Comoros', -12], ['Ivory Coast', 6],
-                ['CD', -3], ['Djibouti', 12],
-                ['Egypt', 26], ['Equatorial Guinea', 3], ['Eritrea', 15],
-                ['Ethiopia', 9], ['Gabon', 0], ['Gambia', 13], ['Ghana', 5],
-                ['Guinea', 10], ['Guinea-Bissau', 12], ['Kenya', -1],
-                ['Lesotho', -29], ['Liberia', 6], ['Libya', 32], ['Madagascar', -12], 
-                ['Malawi', -14], ['Mali', 12], ['Mauritania', 18],
-                ['Mauritius', -20],['Morocco', 32], ['Mozambique', -25], ['Namibia', -22],
-                ['Niger', 14], ['Nigeria', 8], ['Republic of the Congo', -1],
-                ['Rwanda', -2], ['Saint Helena', -16],
-                ['São Tomé and Principe', 1], ['Senegal', 15],
-                ['Seychelles', -5], ['Sierra Leone', 8], ['Somalia', 2],
-                ['Sudan', 15], ['South Africa', -30], ['SS', 5],
-                ['Swaziland', -26], ['Tanzania', -6], ['Togo', 6], ['Tunisia', 34],
-                ['Uganda', 1], ['Western Sahara', 25], ['Zambia', -15],
-                ['Zimbabwe', -18],
+                ['Algeria', 30], ['Angola', -8], ['Benin', 6], ['Botswana', -24],
+                ['Burkina Faso', 12], ['Burundi', -3], ['Cameroon', 3], ['CG', 2], 
+                ['Cape Verde', 15], ['Central African Republic', 4], ['Ceuta', 35], ['Chad', 12],
+                ['Comoros', -12], ['Ivory Coast', 6], ['CD', -3], ['Djibouti', 12],
+                ['Egypt', 26], ['Equatorial Guinea', 3], ['Eritrea', 15], ['Ethiopia', 9], 
+                ['Gabon', 0], ['Gambia', 13], ['Ghana', 5], ['Guinea', 10], 
+                ['Guinea-Bissau', 12], ['Kenya', -1], ['Lesotho', -29], ['Liberia', 6], 
+                ['Libya', 32], ['Madagascar', -12], ['Malawi', -14], ['Mali', 12], 
+                ['Mauritania', 18], ['Mauritius', -20],['Morocco', 32], ['Mozambique', -25], 
+                ['Namibia', -22], ['Niger', 14], ['Nigeria', 8], ['Republic of the Congo', -1],
+                ['Rwanda', -2], ['Saint Helena', -16], ['São Tomé and Principe', 1], ['Senegal', 15],
+                ['Seychelles', -5], ['Sierra Leone', 8], ['Somalia', 2], ['Sudan', 15], 
+                ['South Africa', -30], ['SS', 5], ['Swaziland', -26], ['Tanzania', -6], 
+                ['Togo', 6], ['Tunisia', 34], ['Uganda', 1], ['Western Sahara', 25], 
+                ['Zambia', -15], ['Zimbabwe', -18]
             ]}
 
             options = {{
@@ -40,7 +46,7 @@ function Geochart() {
                 defaultColor: '#ffffff',
             }}
 
-  mapsApiKey=""
+  mapsApiKey={REACT_APP_GOOGLE_MAP_API}
   rootProps={{ 'data-testid': '1' }}
 />
     )
